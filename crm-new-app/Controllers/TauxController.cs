6855ec21fbe41161065ddaf6f67cd00ed8767eb5
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-03-24 08:07:24 +0300

    Configuration Taux

---
+++ crm-new-app/Controllers/TauxController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crm_new_app.Controllers;
+
+public class TauxController : Controller
+{
+    private readonly ILogger<DashboardController> _logger;
+    private readonly HttpClient _httpClient;
+
+    public TauxController(ILogger<DashboardController> logger)
+    {
+        _logger = logger;
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:8080")
+        };
+    }
+
+    public IActionResult Seuil()
+    {
+        return View();
+    }
+
+    [HttpPost("/Taux/UpdateTaux")]
+    public async Task<IActionResult> UpdateTaux(double newTaux)
+    {
+        try
+        {
+            string apiUpdateTaux = $"api/taux/update?newTaux={newTaux}";
+            
+            var response = await _httpClient.PostAsync(apiUpdateTaux, null);
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Taux mis à jour avec succès.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Erreur lors de la mise à jour du montant.";
+            }
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            TempData["ErrorMessage"] = "Erreur lors de la mise à jour du montant.";
+
+        }
+        return RedirectToAction("Seuil");
+    }
+}
--- crm-new-app/obj/Debug/net9.0/crm-new-app.AssemblyInfo.cs
+++ crm-new-app/obj/Debug/net9.0/crm-new-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crm-new-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b6823f19711e9301189dabe0e2bdbf332258f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+083dca08178a71e1b9197a58f6b19f6501dc3835")]
 [assembly: System.Reflection.AssemblyProductAttribute("crm-new-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crm-new-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
